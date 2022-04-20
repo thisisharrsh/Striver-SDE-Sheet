aa351b35a5427149601d952a6d9387a4113354ca
thisisharrsh <88570905+thisisharrsh@users.noreply.github.com>
2022-04-20 23:18:14 +0530

    Time: 2114 ms (18.24%), Space: 13.8 MB (94.79%) - LeetHub

---
+++ 18-4sum/18-4sum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        
+        nums.sort()
+        ans = []
+        st = set()
+        for i in range(len(nums)-2):
+            for j in range(i+1,len(nums)-2):
+                l = j+1
+                r =len(nums)-1
+                while(l<r):
+                    s = nums[i]+nums[j]+nums[l]+nums[r]
+                    if s<target:
+                        l=l+1
+                    elif s>target:
+                        r = r-1
+                    else:
+                        if(nums[i],nums[j],nums[l],nums[r]) not in st:
+                            st.add((nums[i],nums[j],nums[l],nums[r]))
+                            ans.append([nums[i],nums[j],nums[l],nums[r]])
+                        l=l+1
+                        r=r-1
+        return ans
