ac9bb4278fe6d4697c583eafb4c3e7715163925e
thisisharrsh <88570905+thisisharrsh@users.noreply.github.com>
2022-07-11 14:06:23 +0530

    Time: 49 ms (59.17%), Space: 14 MB (81.60%) - LeetHub

---
+++ 78-subsets/78-subsets.py
@@ -0,0 +1,9 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        
+        res=[[]]
+        for i in nums:
+            res+= [j + [i] for j in res]
+            
+        return res
+        
