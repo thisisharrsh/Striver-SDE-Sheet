2541a387aab77a9d2753dfacb84c44ffe33112e3
thisisharrsh <88570905+thisisharrsh@users.noreply.github.com>
2022-07-18 16:50:13 +0530

    Time: 60 ms (20.56%), Space: 13.8 MB (49.30%) - LeetHub

--- 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.py
+++ 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.py
@@ -7,7 +7,18 @@
 class Solution:
     def checkTree(self, root: Optional[TreeNode]) -> bool:
         
-        if(root.val == root.left.val+root.right.val):
-            return(True)
+        lval = 0 
+        rval = 0
+        if(root==None or (root.left==None and root.right == None)):
+            return True
         else:
-            return (False)
+            if(root.left!=None):
+                ldata=root.left.val
+            if(root.right!=None):
+                rdata = root.right.val 
+            if(root.val==rdata+ldata and (self.checkTree(root.right) and self.checkTree(root.left))):
+                return True
+            
+            else:
+                return False
+        
